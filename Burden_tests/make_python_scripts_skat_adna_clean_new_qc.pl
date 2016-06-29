5cab3caa56c7633b1c953f6ea4b4a06197b9b0c3
Claire Malley <clairemalley2015@u.northwestern.edu>
2016-06-29 11:26:03 -0400

    Add files via upload

---
+++ Burden_tests/make_python_scripts_skat_adna_clean_new_qc.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+for(my $c=1; $c<=22; $c++){
+        my $output = "python_scripts_adrn_skat_adna_clean_new_qc_chr".$c.".py";
+        open(OUT, ">$output") or die"Can't open output file\n";
+	print OUT "#!/bin/env python\n";
+        print OUT "import subprocess\n";
+	print OUT "command \= \(\"Rscript\"\, \"\/users\/mboorgu1\/ADRN_AD_NA_SKAT_commands_gene_based_clean_varsets_new_qc\.R\"\, str\(".$c."\)\)";
+	print OUT "\n";
+        print OUT "process \= subprocess\.Popen\(command, stdout\=subprocess\.PIPE\)\n";
+        print OUT "print\(process\.communicate\(\)\[0\]\)\n";
+	}
+
+exit;
+
