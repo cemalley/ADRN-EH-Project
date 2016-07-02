11c090ad5bdfd20a72328512365e72cf94444269
C Malley <clairemalley2015@u.northwestern.edu>
2016-07-01 21:23:56 -0400

    Update picard-qsub.sh

--- GATK/picard-qsub.sh
+++ GATK/picard-qsub.sh
@@ -3,5 +3,5 @@
 
 cat  /users/cmalley/adrn/lplist-usable.txt | while read DATAFILE
 do
-   qsub -cwd -l mem_free=20G,h_vmem=21G,h_fsize=300G picard1.sh $DATAFILE
+   qsub -cwd -l mem_free=20G,h_vmem=21G,h_fsize=300G picard.sh $DATAFILE
 done
