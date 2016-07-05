8df46ff648640dd9ced10fabf65e696d477fb0c5
C Malley <clairemalley2015@u.northwestern.edu>
2016-07-05 15:54:19 -0400

    Update GGPLOT.R

--- Plots/GGPLOT.R
+++ Plots/GGPLOT.R
@@ -4,7 +4,12 @@ install.packages('wesanderson')
 library(wesanderson)
 library(ggplot2)
 
-tt <-theme(legend.text = element_text(size=14), legend.title=element_text(size=16), text=element_text(size=14), axis.text = element_text(size=14))
+tt <- theme(legend.text = element_text(size=14), legend.title=element_text(size=16), text=element_text(size=14), axis.text = element_text(size=14))
 
+# To apply the wesanderson color brewer:
+
+num.fill <- 2 ## However many colors to select
+
+wa <- scale_fill_manual(values=wes_palette(n=num.fill, name="GrandBudapest"), name="Legend title")
 
 ## C Malley Jul 2 2016
