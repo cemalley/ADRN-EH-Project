f7b9c014f6405c245397a3f36ed713640fc6d226
C Malley <clairemalley2015@u.northwestern.edu>
2016-07-06 09:57:16 -0400

    Update picard.sh

--- GATK/picard.sh
+++ GATK/picard.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 i="$1"
-PICARD="/users/cmalley/apps/picard-tools-2.4.1/picard.jar"
 
-m="eval java -d64 -XX:MaxHeapSize=512m -Xms5g -Xmx10g -jar $PICARD MarkDuplicates I=/dcl01/mathias/data/ADRN_EH/${i}/Assembly/${i}.bam O=/dcl01/mathias/data/ADRN_EH/${i}/Assembly/${i}.marked_duplicates.bam M=${i}.marked_dup_metrics.txt VALIDATION_STRINGENCY=LENIENT"
+m="eval java -d64 -Djava.io.tmpdir=`pwd`/tmp -XX:MaxHeapSize=512m -Xms8g -Xmx15g -jar /users/cmalley/apps/picard-tools-2.4.1/picard.jar MarkDuplicates I=/dcl01/mathias/data/ADRN_EH/${i}/Assembly/${i}.bam O=/dcl01/mathias/data/ADRN_EH/${i}/Assembly/${i}.marked_duplicates.bam M=${i}.marked_dup_metrics.txt VALIDATION_STRINGENCY=LENIENT MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=1000 TMP_DIR=`pwd`/tmp"
 
 eval $m
