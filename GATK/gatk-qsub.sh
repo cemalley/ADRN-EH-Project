c07e05442567341874fa9a4decbe343dc73e26d2
sssaini011 <sssaini011@gmail.com>
2016-08-04 15:01:02 -0400

    Update gatk-qsub.sh

--- GATK/gatk-qsub.sh
+++ GATK/gatk-qsub.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # S. Saini August 4 2016
 
-cat  /users/cmalley/adrn/lplist-usable.txt | while read DATAFILE
+usable_list=/users/cmalley/adrn/lplist-usable.txt
+
+cat $usable_list  | while read DATAFILE
 do
 
-   qsub -cwd -l mem_free=20G,h_vmem=21G,h_fsize=300G /users/ssaini/recALL/first_step.sh $DATAFILE
+   qsub -cwd -l mem_free=20G,h_vmem=21G,h_fsize=300G gatk_streamlined.sh $DATAFILE
 
 done
