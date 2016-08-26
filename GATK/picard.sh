5960f03ed2d92d43f69338dfc606a1ad7704371d
C Malley <cmalley3@jhmi.edu>
2016-08-26 10:51:04 -0400

    Update picard.sh

--- GATK/picard.sh
+++ GATK/picard.sh
@@ -4,7 +4,7 @@
 
 i="$1"
 
-m="eval java -d64 -Djava.io.tmpdir=`pwd`/tmp -XX:MaxHeapSize=512m -Xms12g -Xmx13g -jar /users/cmalley/apps/picard-tools-2.4.1/picard.jar MarkDuplicates I=/dcl01/mathias/data/ADRN_EH/${i}/Assembly/${i}.bam O=/dcl01/mathias/data/ADRN_EH/${i}/Assembly/${i}.marked_duplicates.bam M=${i}.marked_dup_metrics.txt VALIDATION_STRINGENCY=LENIENT MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=1000 TMP_DIR=`pwd`/tmp"
+m="eval java -d64 -Djava.io.tmpdir=`pwd`/tmp -XX:MaxHeapSize=512m -Xms12g -Xmx13g -jar /users/cmalley/apps/picard-tools-2.4.1/picard.jar MarkDuplicates I=/dcl01/mathias/data/ADRN_EH/${i}/Assembly/${i}.bam O=/dcl01/mathias/data/ADRN_EH/${i}/Assembly/${i}.marked_duplicates.bam M=${i}.marked_dup_metrics.txt VALIDATION_STRINGENCY=LENIENT MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=1000 TMP_DIR=`pwd`/tmp REMOVE_DUPLICATES=true"
 
 eval $m
 
