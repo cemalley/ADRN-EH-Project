5f01ae50b2db2c56d08be54a0fdebcfe6f771183
C Malley <cmalley3@jhmi.edu>
2017-03-29 15:08:30 -0400

    Update bcftools-filter.sh

--- VCF_processing/bcftools-filter.sh
+++ VCF_processing/bcftools-filter.sh
@@ -1,6 +1,6 @@
 # Notes by Claire Malley, March 29, 2017, on how to filter a VCF with bcftools, on site-stats, as well as INFO and FORMAT fields.
 
-$ produces a segfault:
+# produces a segfault:
 bcftools filter -i '%QUAL>20 | INFO/TR>7 | FMT/NR[*]>7' -O v ADRN_EDC_799_platypus_maxvar8_minreads5.norm.vcf.gz > ADRN_EDC_799_platypus_maxvar8_minreads5.norm.QC.GQTRNR.vcf
 
 # works:
